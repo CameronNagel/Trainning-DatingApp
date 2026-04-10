acff649ab41677f07da3f4de7167011ada54da39
Cameron-Nagel <cameronnagel8000@gmail.com>
2026-04-10 19:30:28 +0200

    pull request

---
+++ API/Program.cs
@@ -0,0 +1,41 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<Data.AppDbContext>(opt =>
+            {
+                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseCors(x => x
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
+
